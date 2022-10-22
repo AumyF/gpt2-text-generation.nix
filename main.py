12837d3435114da12fc8d674560868718adf7290
Aumy <aumy@outlook.jp>
2022-10-23 00:33:17 +0900

    Use pipeline

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import os
-from transformers import T5TokenizerFast, GPT2LMHeadModel
+from transformers import T5TokenizerFast, GPT2LMHeadModel, pipeline
 
-PREFIX_TEXT = "こんなときに貴女は"
+max_length = os.environ.get('MAX_LENGTH', 140)
 
+prefix_text = os.environ.get('PREFIX_TEXT', "風をずっと待っているの 身軽になるための言葉自体が重くて歩けないわ") # Inaba Kumori - Kazemachigusa
+
+# provided from nix-shell
 model_path = os.environ['MODELPATH']
 
 tokenizer = T5TokenizerFast.from_pretrained(model_path)
-tokenizer.do_lower_case = True
-
 model = GPT2LMHeadModel.from_pretrained(model_path)
 
+generator = pipeline('text-generation', model = model, tokenizer = tokenizer)
+generator.model.config.pad_token_id = generator.model.config.eos_token_id
+outputs = generator(prefix_text, max_length=max_length)
 
-input = tokenizer.encode(PREFIX_TEXT, return_tensors="pt")
-output = model.generate(input, do_sample=True, max_length=140, num_return_sequences=1)
-
-for out in (tokenizer.batch_decode(output)):
-    print(out)
+for output in outputs:
+    print(output)
